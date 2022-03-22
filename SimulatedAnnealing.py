76fbc9c13952ce66ad55e5efc6204854241b890b
KeeganBruer <keegan.bruer@gmail.com>
2022-03-22 16:00:16 -0400

    Added Simulated Annealing Algorithm

--- SimulatedAnnealing.py
+++ SimulatedAnnealing.py
@@ -8,42 +8,32 @@ class SimulatedAnnealing:
         self.alpha = alpha
 
     def anneal(self, initial_state):
-        """Peforms simulated annealing to find a solution"""
-        initial_temp = self.initial_temp
-        final_temp = self.final_temp
-        alpha = self.alpha
-        
-        current_temp = initial_temp
-
-        # Start by initializing the current state with the initial state
+        """Peforms simulated annealing to find a solution"""        
+        # Start by initializing the current temp and state
+        self.current_temp = self.initial_temp
         self.current_state = initial_state
-        solution = self.current_state
 
-        while current_temp > final_temp:
+        while self.current_temp > self.final_temp:
+            # decrement the temperature
+            self.current_temp -= self.alpha
+            ##get neighbor and cost difference
             neighbor = random.choice(self.get_neighbors(self.current_state))
-
-            # Check if neighbor is best so far
             cost_diff = self.get_cost(self.current_state) - self.get_cost(neighbor)
 
             # if the new solution is better, accept it
             if cost_diff > 0:
-                solution = neighbor
+                self.current_state = neighbor
+                continue
             # if the new solution is not better, accept it with a probability of e^(-cost/temp)
-            else:
-                if random.uniform(0, 1) < math.exp(-cost_diff / current_temp):
-                    solution = neighbor
-
-            self.current_state = solution
-            print(self.current_state)
-            # decrement the temperature
-            current_temp -= alpha
-
+            if random.uniform(0, 1) < math.exp(-cost_diff / self.current_temp):
+                self.current_state = neighbor
+                continue            
         return solution
 
     def get_cost(self, state):
-        """Calculates cost of the argument state for your solution."""
+        """Calculates cost of the current state."""
         raise NotImplementedError
         
     def get_neighbors(self, state):
-        """Returns neighbors of the argument state for your solution."""
+        """Returns neighbors of the current state"""
         raise NotImplementedError
