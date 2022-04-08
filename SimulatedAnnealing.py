80fed79939149193d353a49db22e3696d269f07f
KeeganBruer <keegan.bruer@gmail.com>
2022-04-08 14:25:39 -0400

    Full Sample Training

--- SimulatedAnnealing.py
+++ SimulatedAnnealing.py
@@ -2,10 +2,11 @@ import random
 import math
 
 class SimulatedAnnealing:
-    def __init__(self, initial_temp = 90, final_temp = 0.1, alpha = 0.01):
+    def __init__(self, initial_temp = 90, final_temp = 0.1, alpha = 0.1, per_temp=1):
         self.initial_temp = initial_temp
         self.final_temp = final_temp
         self.alpha = alpha
+        self.per_temp = per_temp
 
     def anneal(self, initial_state):
         """Peforms simulated annealing to find a solution"""        
@@ -15,16 +16,27 @@ class SimulatedAnnealing:
 
         while self.current_temp > self.final_temp:
             ##get neighbor and cost difference
-            neighbor = random.choice(self.get_neighbors(self.current_state, self.current_temp ))
-            cost_diff = self.get_cost(self.current_state, True) - self.get_cost(neighbor, False)
-
-            # if the new solution is better, accept it
-            if cost_diff > 0:
-                self.current_state = neighbor                
-            # if the new solution is not better, accept it with a probability of e^(-cost/temp)
-            elif random.uniform(0, 1) < math.exp(cost_diff / self.current_temp):
-                self.current_state = neighbor
-            # decrement the temperature
+            neighbors = self.get_neighbors(self.current_state, self.current_temp )
+            curr_cost = self.get_cost(self.current_state, True) 
+            for i in range(self.per_temp):
+                neighbor = random.choice(neighbors)
+                neighbor_cost = self.get_cost(neighbor, False)
+                cost_diff = curr_cost - neighbor_cost
+                try:
+                    prob = math.exp(-(neighbor_cost-curr_cost) / self.current_temp)
+                except OverflowError:
+                    prob = 0 #If out of bounds don't accept solution.
+                
+                print("T ", cost_diff, self.current_temp, prob)
+                # if the new solution is better, accept it
+                if cost_diff >= 0:
+                    self.current_state = neighbor    
+                    curr_cost = neighbor_cost          
+                # if the new solution is not better, accept it with a probability of e^(-cost/temp)
+                elif random.uniform(0, 1) < prob:
+                    self.current_state = neighbor
+                    curr_cost = neighbor_cost #Save neighbor cost instead of recalculating cost of new current state 
+                # decrement the temperature
             self.current_temp -= self.alpha
         return self.current_state
 
--- run_annealing_test.py
+++ run_annealing_test.py
@@ -13,22 +13,23 @@ def get_sample(count, *args):
 raw_data = numpy.load("./training_data/dataset_out.npz")
 x = raw_data["X"]
 y = raw_data["Y"]
-x, y = get_sample(200000, x, y)
+#x, y = get_sample(20000, x, y)
 print(x.shape)
 
 target_x = [0, 0, 2, 0, 0.1, 0, 4, 0, 5, 0, -0.1, -1]
 initial_z = numpy.random.rand(12)
+print("Initial Z", initial_z)
 #[0, 0, 1, 0, 0.02, 0, 2, 0, 5, 0, 0.4, 0]
 
 from SimulatedAnnealing import SimulatedAnnealing
 params = None
 sphere_radius = 1
 distance_error = 0.002
-sim_an = SimulatedAnnealing(initial_temp = 90)
+sim_an = SimulatedAnnealing(initial_temp = 30, final_temp = 0.001, alpha = 0.001, per_temp=1)
 
 
 def cost_func(state, isCurrent):
-    #target_diff = numpy.array(state) - numpy.array(target_x)
+    target_diff = numpy.array(state) - numpy.array(target_x)
     y_out, _ = compute_mu_and_sigma(x, state)
     diff =  y - y_out
     error = numpy.sum(numpy.absolute(diff))
@@ -39,7 +40,7 @@ def cost_func(state, isCurrent):
 sim_an.get_cost = cost_func
 def neighbors_func(state, temp):
     #cov = numpy.random.rand(len(state), len(state)) 
-    change = 1 * (temp/10)
+    change = temp 
     cov = numpy.eye(len(state)) * change
     rtn = numpy.random.multivariate_normal(state, cov, 100)
     return rtn
